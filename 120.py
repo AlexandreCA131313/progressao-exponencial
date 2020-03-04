d64dcbc21f4867bf9d36838e5ab5662fd70a3591
Alexandre Camargo de Almeida <60950415+AlexandreCA131313@users.noreply.github.com>
2020-03-03 22:32:37 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,11 @@
+# AUTORES: 1DSI3
+# @ALEXANDRE
+# Feito em: 03/03/2020
+# Instituição: ETEC
+x = 0 # Declaração de comando
+valor = 2 # Declaração da variavel valor
+while (x <= 21):
+	    valor = valor * valor
+	    x = x + 1 # Laço de repetição
+print (valor) # Exibir a variavel valor	    
+	
